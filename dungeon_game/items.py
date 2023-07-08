48bef6e9313c3c3c7ac65d1c7519779f76356025
Zachary <rollio@zac-den.com>
2023-07-07 20:24:50 -0500

    changed a few things and added somemore

--- dungeon_game/items.py
+++ dungeon_game/items.py
@@ -5,6 +5,7 @@ class inventory:
         self.inv = [slot(), slot(), slot()]
     
     def get_item(self, item):
+        c = 0
         for i in range(0, 2):
             if len(self.inv[i].stack) == 0 :
                 self.inv[i].stack.append(item)
@@ -14,7 +15,21 @@ class inventory:
                 break
             else:
                 pass
-
+            c = c +1
+        if c == 3:
+            print('slots full!')
+    
+    def clear_slot(self, slot_num):
+        self.inv[int(slot_num)].stack.clear()
+        print(f'{slot_num} cleared!')
+    
+"""     def use_item(self, item):
+            try:
+                self.inv[int(item)].stack[0].use()
+                self.inv[int(item)].stack.remove(item)
+            except:
+                pass """
+# to be implemented
 
 class slot:
         def __init__(self):
